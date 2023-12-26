52e51f71a99ce19a62218d35bbeb12784da2b608
mohammaduzer07 <134217616+mohammaduzer07@users.noreply.github.com>
2023-12-26 20:33:55 +0530

    Controller debug

--- src/main/java/com/driver/StudentController.java
+++ src/main/java/com/driver/StudentController.java
@@ -48,21 +48,21 @@ public class StudentController {
     @GetMapping("/get-teacher-by-name/{name}")
     public ResponseEntity<Teacher> getTeacherByName(@PathVariable String name){
         Teacher teacher = null; // Assign student by calling service layer method
-        studentService.getTeacherByName(name);
+        teacher = studentService.getTeacherByName(name);
         return new ResponseEntity<>(teacher, HttpStatus.CREATED);
     }
 
     @GetMapping("/get-students-by-teacher-name/{teacher}")
     public ResponseEntity<List<String>> getStudentsByTeacherName(@PathVariable String teacher){
         List<String> students = null; // Assign list of student by calling service layer method
-        studentService.getStudentByTeacherName(teacher);
+        students = studentService.getStudentByTeacherName(teacher);
         return new ResponseEntity<>(students, HttpStatus.CREATED);
     }
 
     @GetMapping("/get-all-students")
     public ResponseEntity<List<String>> getAllStudents(){
         List<String> students = null; // Assign list of student by calling service layer method
-        studentService.getAllStudent();
+       students = studentService.getAllStudent();
         return new ResponseEntity<>(students, HttpStatus.CREATED);
     }
 
